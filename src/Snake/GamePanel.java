02a7bb9388b46b75e212a28fd58ff94b461a0663
Fabian Zbrański <fabian.zbranski@gmail.com>
2021-12-08 16:49:52 +0100

    Create all methods

--- src/Snake/GamePanel.java
+++ src/Snake/GamePanel.java
@@ -1,8 +1,10 @@
 package Snake;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
 import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
@@ -19,7 +21,39 @@ public class GamePanel extends JPanel implements ActionListener {
     boolean running = false;
     Timer timer;
     Random random;
+    GamePanel(){
+        this.setPreferredSize(new Dimension(SCREEN_HEIGHT,SCREEN_WIDTH));
+        this.setBackground(Color.black);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter());
+    }
+    public void startGame(){
 
+    }
+    public void paintComponent(Graphics g){
+
+    }
+    public void draw(Graphics g){
+
+    }
+    public void newApple(){
+
+    }
+    public void move(){
+
+    }
+    public void checkApple(){
+
+    }
+    public void checkCollisions(){
+
+    }
+    public void gameOver(){
+
+    }
+    public class MyKeyAdapter extends KeyAdapter{
+
+    }
     @Override
     public void actionPerformed(ActionEvent e) {
 
