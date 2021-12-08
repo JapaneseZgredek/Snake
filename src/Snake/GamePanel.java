314da7de550bc83b14e3fda123e0a27412646992
Fabian Zbrański <fabian.zbranski@gmail.com>
2021-12-08 16:44:33 +0100

    create all var that we need

--- src/Snake/GamePanel.java
+++ src/Snake/GamePanel.java
@@ -1,6 +1,27 @@
 package Snake;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements ActionListener {
+
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (SCREEN_HEIGHT*SCREEN_WIDTH)/UNIT_SIZE;
+    static final int DELAY = 75;
+    final int[] x = new int[GAME_UNITS];
+    final int[] y = new int[GAME_UNITS];
+    int bodyParts = 6, applesEaten = 0, appleX, appleY;
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
+    Random random;
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
