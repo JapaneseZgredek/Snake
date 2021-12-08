3e435dc0eb5e2d444ab5a418a08c2c0dcf8e4b65
Fabian Zbrański <fabian.zbranski@gmail.com>
2021-12-08 16:40:33 +0100

    add comments to GameFrame

--- src/Snake/GameFrame.java
+++ src/Snake/GameFrame.java
@@ -4,12 +4,12 @@ import javax.swing.*;
 
 public class GameFrame extends JFrame {
     GameFrame(){
-        this.add(new GamePanel()); 
-        this.setTitle("Snake Game");
-        this.setResizable(false);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.pack();
-        this.setVisible(true);
-        this.setLocationRelativeTo(null);
+        this.add(new GamePanel());  // Adding Panel to our Frame
+        this.setTitle("Snake Game"); //Setting title
+        this.setResizable(false); // our frame wont be resizable
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // our application will close when we close the window
+        this.pack(); // this will pack around our components that we add to the frame
+        this.setVisible(true); // shows window on our screen
+        this.setLocationRelativeTo(null); // window will appear in the center of our window
     }
 }
