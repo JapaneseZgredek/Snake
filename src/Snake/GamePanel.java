1b9132ec213245599812becd493befdd57f5517d
Fabian Zbrański <fabian.zbranski@gmail.com>
2021-12-08 17:02:34 +0100

    Create code for checkApple

--- src/Snake/GamePanel.java
+++ src/Snake/GamePanel.java
@@ -69,7 +69,11 @@ public class GamePanel extends JPanel implements ActionListener {
         }
     }
     public void checkApple(){
-
+        if(x[0] == appleX && y[0] == appleY){
+            bodyParts++;
+            applesEaten++;
+            newApple();
+        }
     }
     public void checkCollisions(){
 
@@ -83,7 +87,9 @@ public class GamePanel extends JPanel implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if(running){
-            newApple();
+            move();
+            checkApple();
+            checkCollisions();
         }
     }
 }
