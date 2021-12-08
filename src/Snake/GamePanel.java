2d3cf77b096b8a5a3ed6c495942ee94588ae2c2a
Fabian Zbrański <fabian.zbranski@gmail.com>
2021-12-08 17:00:05 +0100

    Create code for move method

--- src/Snake/GamePanel.java
+++ src/Snake/GamePanel.java
@@ -50,7 +50,23 @@ public class GamePanel extends JPanel implements ActionListener {
         appleY = random.nextInt(SCREEN_HEIGHT/UNIT_SIZE)*UNIT_SIZE;
     }
     public void move(){
-
+        for(int i=bodyParts; i>0; i--){
+            x[i] = x[i-1];
+            y[i] = y[i-1];
+        }
+        switch (direction){
+            case 'R':
+                x[0] = x[0] + UNIT_SIZE;
+                break;
+            case 'L':
+                x[0] = x[0] - UNIT_SIZE;
+                break;
+            case 'U':
+                y[0] = y[0] - UNIT_SIZE;
+                break;
+            case 'D':
+                y[0] = y[0] + UNIT_SIZE;
+        }
     }
     public void checkApple(){
 
